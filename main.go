3f13072cad9bdf43fd26e1474dbdd5a444f178f6
Nathan Fisher <nfisher@junctionbox.ca>
2018-10-16 11:40:12 -0300

    Add Event.Links for use with event_call

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"google.golang.org/appengine"
@@ -13,14 +14,14 @@ func main() {
 }
 
 func handleEvents(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var event Event
-
-	defer r.Body.Close()
+	var event Message
 
 	dec := json.NewDecoder(r.Body)
 
@@ -35,16 +36,29 @@ func handleEvents(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "text/plain")
 		w.Write([]byte(event.Challenge))
 		return
-
+	case "event_callback":
+		log.Println(event.Event.Type)
+		return
 	}
 }
 
-type Event struct {
+type Message struct {
 	Token       string   `json:"token"`
 	Challenge   string   `json:"challenge,omitempty"`
 	Type        string   `json:"type"`
 	TeamID      string   `json:"team_id"`
-	ApiAppID    string   `json:"api_app_id"`
+	APIAppID    string   `json:"api_app_id"`
 	AuthedUsers []string `json:"authed_users"`
 	EventID     string   `json:"event_id"`
+	Event       Event    `json:"event,omitempty"`
+}
+
+type Event struct {
+	Type  string `json:"type"`
+	Links []Link `json:"links"`
+}
+
+type Link struct {
+	Domain string `json:"domain"`
+	URL    string `json:"url"`
 }
